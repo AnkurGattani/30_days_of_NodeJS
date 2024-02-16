2ca9918df07c5a59cae9208ef688fb622314be2f
Ankur Gattani <ankurgattani02@gmail.com>
2024-02-17 00:06:56 +0530

    Added Day16

---
+++ Day16/Day16_Solution.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+async function connectToDatabase() {
+	try {
+		await mongoose.connect('mongodb://127.0.0.1:27017/');
+		console.log('Connected to MongoDB database');
+	} catch (error) {
+		console.error('Error connecting to MongoDB:');
+	}
+}
+
+connectToDatabase();
+
+const PORT = 3300;
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
